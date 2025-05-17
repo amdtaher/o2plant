2556c3d8715a4284c79389452730e34d6293365d
Tanveer Ahmed Taher <101932506+amdtaher@users.noreply.github.com>
2025-05-17 23:53:30 +0600

    Update page.js

--- src/app/O2/page.js
+++ src/app/O2/page.js
@@ -8,7 +8,7 @@ import {
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel"
+} from "@/components/ui/Carousel"
 
 const page = () => {
   const [activeIndex, setActiveIndex] = useState(0)
